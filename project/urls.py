5b5bc99347c3d9337cd68ccb25255fae26bb1e20
BhishanSharma <bhishansharma2@gmail.com>
2025-06-09 20:27:42 +0530

    endpoints editted

--- project/urls.py
+++ project/urls.py
@@ -33,9 +33,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user_app.urls')),
-    path('api/job/', include('job_app.urls')),
-    path('api/apply/', include('application_app.urls')),
+    path('', include('user_app.urls')),
+    path('job/', include('job_app.urls')),
+    path('', include('application_app.urls')),
 
     # Swagger URLs
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
